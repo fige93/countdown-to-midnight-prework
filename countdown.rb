a1fc5f5ca2254c74251771fffdc8f2e28d3f4749
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-17 21:32:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(int)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  start = int
+  while start > 0
+    puts "#{start} SECOND(S)!"
+    start -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
